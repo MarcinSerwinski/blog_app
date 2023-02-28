3b0a9908aa2aa5c81f1b7b14f1d3e804141be246
MarcinSerwinski <MarcinSerwinski@github.com>
2023-02-28 20:22:51 +0100

    views for listing posts and getting details

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'home/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request):
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+    return render(request,
+                  'home/post_detail.html',
+                  {'posts': post})
