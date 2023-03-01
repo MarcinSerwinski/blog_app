a3ed0f9699a945a2748315b417fd69dcb3d2a807
MarcinSerwinski <MarcinSerwinski@github.com>
2023-03-01 18:32:49 +0100

    extended args of get_absolute_method to match the new URL parameters

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -40,4 +40,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                       args=[self.id])
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
