ff76e04c22608c6f2fe767623ca1b1987926618f
MarcinSerwinski <MarcinSerwinski@github.com>
2023-03-09 18:02:38 +0100

    swapped to postgresql

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'xxxxxx',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
