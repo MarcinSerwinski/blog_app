0eac9c3e0bdf695d5325646596e4d088574f4b7e
MarcinSerwinski <MarcinSerwinski@github.com>
2023-03-09 12:27:54 +0100

    posts can be filtered and displayed by tags

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,19 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 from blog.forms import EmailPostForm, CommentForm
 from blog.models import Post
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -19,7 +25,8 @@ def post_list(request):
         posts = paginator.page(1)
     return render(request,
                   'home/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
